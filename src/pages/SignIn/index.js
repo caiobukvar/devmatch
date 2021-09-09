5af45457e19322e09a0b52eac4eeef4d254a1b79
Caio Bukvar <bukvar2009@gmail.com>
2021-09-09 20:08:06 -0300

    criando estilos SignIn e adicionando customizacao global

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
+import './styles.css';
+import '../../styles/form.css';
+import { Link } from 'react-router-dom';
 
 function SignIn() {
     return (
-        <h1>Sign In</h1>
+        <div className="container-form">
+            <form className="form-sign-in">
+                <div className="text-center mb-lg">
+                    <h1>Sign In</h1>
+                    <Link to="/sign-up">Cadastre-se</Link>
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="email">E-mail</label>
+                        <input id="email" type="text" placeholder="Digite seu e-mail" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">E-mail</label>
+                        <input id="password" type="text" placeholder="Digite sua senha" />
+                    </div>
+                    <button className="btn-dark-blue">Entrar</button>
+                    <div>
+                        <input type="checkbox" value="Lembrar-me" name="remember" />
+                        <span htmlFor="remember">Lembrar-me</span>
+                    </div>
+                </div>
+                <Link to="/recpvery-password" className="my-lg">Esqueceu a senha?</Link>
+                <span className="light-label">©2021 Todos os Direitos Reservados</span>
+            </form>
+        </div>
+
     )
 }
 
