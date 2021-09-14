bbd8a1dd5c75dbec3a8e0c50c36aec2f1a72d9dc
Caio Bukvar <bukvar2009@gmail.com>
2021-09-14 19:39:42 -0300

    dinamizando componente JobOpportunity

--- src/components/JobOpportunity/index.js
+++ src/components/JobOpportunity/index.js
@@ -1,25 +1,44 @@
 import './styles.css';
 import Logo from '../../assets/google-logo.svg';
 
-function JobOpportunity() {
+function JobOpportunity({
+    img_url,
+    name,
+    description,
+    requirements,
+    seniority,
+    salary,
+    benefits
+}) {
     return (
         <div className="container-job-opportunity">
-            <img src={Logo} alt="logo" className="logo" />
-            <h2>Google</h2>
+            <img src={img_url} alt="logo" className="logo" />
+            <h2>{name}</h2>
             <div className="details">
                 <strong>Descrição</strong>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et pellentesque risus tempor blandit. Eu nibh et at dignissim diam, donec adipiscing. Suspendisse imperdiet mauris in enim diam, tortor id aliquet sagittis. Aliquam arcu, et proin phasellus porta aliquet suscipit. Sed nunc, nam sem placerat aliquam vitae vel hendrerit ullamcorper. Vel faucibus quisque diam pulvinar quam. Neque nibh est tellus lacus malesuada egestas volutpat sit vitae. Nunc mollis pellentesque netus sollicitudin id egestas convallis. Posuere tellus amet ultricies arcu commodo vitae in in. Sed bibendum enim at ut condimentum pulvinar vulputate suspendisse diam. Porta tempus ornare ac nulla tristique eleifend libero cursus. Pellentesque felis ornare gravida viverra cursus eget curabitur feugiat. Urna, velit ornare vitae facilisi egestas est. Amet nibh porttitor scelerisque felis felis enim. Id pellentesque ut porta nec sed a, cursus. Platea sem odio ornare ut varius fermentum, facilisis id. A, amet ut sit eu nisl. Mattis molestie urna lacus aliquet tempus semper a sapien. Eget aliquet risus commodo, nulla tellus. Sem et in in diam enim, nunc erat egestas leo. Enim id ultrices platea habitant quis egestas. Duis gravida quam molestie.</p>
+                <p>{description}</p>
             </div>
             <div className="details">
                 <strong>Requisitos</strong>
+                <div className="flex-row">
+                    {requirements.map(requirement => (
+                        <div className="badge-blue">
+                            {requirement}
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="details">
                 <strong>Senioridade</strong>
-                <span>Pleno</span>
+                <span>{seniority}</span>
             </div>
             <div className="details">
                 <strong>Salário / Benefícios</strong>
-                <span>R$ 5.200,00 / VT, VA, Plano de saúde, Gympass, Auxílio Home Office</span>
+                <span>
+                    R$ {salary} / {benefits.map((item, index) => (
+                        <span>{item} {index + 1 < benefits.length && ', '}</span>
+                    ))}
+                </span>
             </div>
         </div>
     );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,7 +6,15 @@ function Main() {
   return (
     <div className="container-main">
       <Profilebar />
-      <JobOpportunity />
+      <JobOpportunity
+        img_url="https://e7.pngegg.com/pngimages/607/104/png-clipart-googleplex-google-logo-google-search-oliver-atom-text-logo-thumbnail.png"
+        name="Google"
+        description="asdasddasdas gugou"
+        requirements={['React', 'NodeJS', 'ExpressJS']}
+        seniority="Pleno"
+        salary="5.200,00"
+        benefits={['VT', 'VA', 'Plano de saúde', "Gympass", "Auxílio Home Office"]}
+      />
     </div>
   );
 }
